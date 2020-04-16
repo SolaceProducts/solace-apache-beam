a87095915ca8b5cec69dc142a1fc42255723f398
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2020-04-16 14:16:49 -0400

    fix run-samples-tests.sh

--- ci/run-samples-tests.sh
+++ ci/run-samples-tests.sh
@@ -2,17 +2,21 @@
 
 set -e
 
-testRecordApp() {
-  echo
-  echo 'RUN SAMPLE TEST: SolaceRecordTest'
-  echo '------------------------------------------------'
-  echo
-
-  Q_1="Q/fx-$(tr -cd '[:alnum:]' < /dev/urandom | head -c10)-001"
-
+createQueue() {
+  QUEUE="$1"
+  SOLACE_MGMT_URI="https://${SOLACE_HOST}:943/SEMP/v2"
   curl -X POST -H "content-type:application/json" -u "${SOLACE_MGMT_USERNAME}:${SOLACE_MGMT_PASSWORD}" \
-      "https://${SOLACE_HOST}:943/SEMP/v2/config/msgVpns/${SOLACE_VPN_NAME}/queues" \
-      -d "{\"queueName\":\"${Q_1}\",\"egressEnabled\":true,\"ingressEnabled\":true,\"permission\":\"delete\"}"
+      "${SOLACE_MGMT_URI}/config/msgVpns/${SOLACE_VPN_NAME}/queues" \
+      -d "{\"queueName\":\"${QUEUE}\",\"egressEnabled\":true,\"ingressEnabled\":true,\"permission\":\"delete\"}"
+
+  echo "echo Deleting queue ${QUEUE}" >> "$CI_CLEANUP"
+  echo "curl -X DELETE -u \"${SOLACE_MGMT_USERNAME}:${SOLACE_MGMT_PASSWORD}\" ${SOLACE_MGMT_URI}/config/msgVpns/${SOLACE_VPN_NAME}/queues/${QUEUE}" >> "$CI_CLEANUP"
+}
+
+publishMessages() {
+  PQL="$1"
+  MSG_NUM="${2:-100}"
+  MSG_RATE="${3:-10}"
 
   curl -O https://sftp.solace.com/download/SDKPERF_C_LINUX64
   tar -xvf SDKPERF_C_LINUX64
@@ -21,24 +25,53 @@ testRecordApp() {
       -cu="${SOLACE_USERNAME}@${SOLACE_VPN_NAME}" \
       -cp="${SOLACE_PASSWORD}" \
       -mt=persistent \
-      -mn=100 \
-      -mr=10 \
+      -mn="${MSG_NUM}" \
+      -mr="${MSG_RATE}" \
       -pfl=README.md \
-      -pql="${Q_1}"
+      -pql="${PQL}"
+}
 
+testRecordApp() {
+  echo
+  echo 'RUN SAMPLE TEST: SolaceRecordTest'
+  echo '------------------------------------------------'
+  echo
+
+  QUEUE="Q-fx-$(tr -cd '[:alnum:]' < /dev/urandom | head -c10)-001"
+
+  createQueue "$QUEUE"
+  publishMessages "$QUEUE"
+
+  RUNNING_PID="testRecordApp-proc.pid"
   mvn -e compile exec:java \
       -pl solace-apache-beam-samples \
       -Dexec.mainClass=com.solace.apache.beam.examples.SolaceRecordTest \
-      -Dexec.args="--output=README10.counts --cip=tcp://${SOLACE_HOST}:55555 --cu=${SOLACE_USERNAME} --vpn=${SOLACE_VPN_NAME} --cp=${SOLACE_PASSWORD} --sql=${Q_1}" \
+      -Dexec.args="--output=README10.counts --cip=tcp://${SOLACE_HOST}:55555 --cu=${SOLACE_USERNAME} --vpn=${SOLACE_VPN_NAME} --cp=${SOLACE_PASSWORD} --sql=${QUEUE}" \
       2>&1 | tee output.log &
+  echo $! > "$RUNNING_PID"
 
-  while [[ ! -s output.log ]] && ! grep -qE 'BUILD SUCCESS|BUILD FAILURE' output.log; do
-    echo "$(date) Waiting for compile to complete and runner launch"
+  for attempts in {1..30}; do
+    if [[ -s output.log ]]; then
+      if grep -q 'BUILD FAILURE' output.log; then
+        cat output.log
+        >&2 echo Failed to start SolaceTestRecord
+        exit 1
+      elif grep -qE "SolaceRecordTest - \*\*\*CONTRIBUTING. [0-9]+" output.log; then
+        break
+      fi
+    fi
+
+    if [[ "$attempts" == "30" ]]; then
+      cat output.log
+      >&2 echo Timed out while testing SolaceRecordTest
+      exit 1
+    fi
+
+    echo "$(date) Waiting for output"
     sleep 5
   done
 
-  sleep 20
-  grep -E "SolaceRecordTest - \*\*\*CONTRIBUTING. [0-9]+" output.log
+  kill -9 "$(cat "$RUNNING_PID")"
   cat output.log
 }
 
