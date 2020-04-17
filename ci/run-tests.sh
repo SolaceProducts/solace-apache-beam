a309b84ab51dc7d352406e54fbeffbeaa6a893a6
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2020-04-17 11:13:20 -0400

    support Dataflow IT in Travis

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -20,6 +20,15 @@ elif [[ -z "$SOLACE_MGMT_USERNAME" ]]; then
 elif [[ -z "$SOLACE_MGMT_PASSWORD" ]]; then
   >&2 echo no SOLACE_MGMT_PASSWORD env found
   exit 1
+elif [[ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
+  >&2 echo no GOOGLE_APPLICATION_CREDENTIALS env found
+  exit 1
+elif [[ -z "$DATAFLOW_PROJECT" ]]; then
+  >&2 echo no DATAFLOW_PROJECT env found
+exit 1
+  elif [[ -z "$DATAFLOW_TEMP_ROOT" ]]; then
+  >&2 echo no DATAFLOW_TEMP_ROOT env found
+  exit 1
 fi
 
 echo
@@ -29,9 +38,9 @@ echo
 
 mvn clean compile verify -Pit
 
-#echo
-#echo 'RUN INTEGRATION TESTS'
-#echo '------------------------------------------------'
-#echo
-#
-#mvn clean compile verify -Pit -DbeamTestPipelineOptions='[\"--runner=TestDataflowRunner\"]'
+echo
+echo 'RUN INTEGRATION TESTS'
+echo '------------------------------------------------'
+echo
+
+mvn clean compile verify -Pit -DbeamTestPipelineOptions='["--runner=TestDataflowRunner"]'
