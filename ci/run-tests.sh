dead7f85e38f119200a639bd4f02e15f3cc57f9b
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2021-03-22 10:55:24 -0400

    migrate CI from travis to github actions

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -23,10 +23,7 @@ elif [[ -z "$SOLACE_MGMT_PASSWORD" ]]; then
 elif [[ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
   >&2 echo no GOOGLE_APPLICATION_CREDENTIALS env found
   exit 1
-elif [[ -z "$DATAFLOW_PROJECT" ]]; then
-  >&2 echo no DATAFLOW_PROJECT env found
-exit 1
-  elif [[ -z "$DATAFLOW_TEMP_ROOT" ]]; then
+elif [[ -z "$DATAFLOW_TEMP_ROOT" ]]; then
   >&2 echo no DATAFLOW_TEMP_ROOT env found
   exit 1
 fi
